eb8938f76ff07cdb70d059cdcbf1a8514b70143d
TimurRassolov <timur@parsiq.net>
2022-01-10 18:28:42 +0200

    test

--- src/main/java/com/example/internship/controller/MainController.java
+++ src/main/java/com/example/internship/controller/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 
-    @GetMapping("/hello")
+    @GetMapping("/hello1")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
